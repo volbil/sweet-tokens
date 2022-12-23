8d215ca3fc940375e4e470d068a59385ef9d829c
volbil <volbilnexus@gmail.com>
2022-12-23 18:40:50 +0200

    Tokens list

--- service/layer/views.py
+++ service/layer/views.py
@@ -1,5 +1,5 @@
+from ..models import Block, Token
 from fastapi import APIRouter
-from ..models import Block
 
 router = APIRouter(prefix="/layer")
 
@@ -12,3 +12,25 @@ async def latest():
         "height": latest.height,
         "hash": latest.hash,
     }
+
+@router.get("/tokens")
+async def latest():
+    tokens = await Token.filter()
+    result = []
+
+    for token in tokens:
+        balances = await token.balances.filter(value__gt=0).count()
+        transfers = await token.transfers.filter().count()
+        owner = await token.owner
+
+        result.append({
+            "reissuable": token.reissuable,
+            "decimals": token.decimals,
+            "transfers": transfers,
+            "supply": token.supply,
+            "ticker": token.ticker,
+            "owner": owner.label,
+            "balances": balances
+        })
+
+    return result
